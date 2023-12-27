da3e68c597b4829ea99c9cd82960124c03e0e9a3
daniel diamant <danieldia44@gmail.com>
2023-12-27 10:25:03 +0200

    table and pagination using material

--- client/src/app/users/users-table/users-table.component.ts
+++ client/src/app/users/users-table/users-table.component.ts
@@ -1,6 +1,9 @@
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { User } from '../../models/user.model';
 import { UsersService } from './../users.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-users-table',
@@ -8,63 +11,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './users-table.component.scss',
 })
 export class UsersTableComponent implements OnInit {
-  users: User[] = [];
   sortOrder: string = '';
   filterdUsers: User[] = [];
-  page: number = 1;
-  pageSize: number = 10;
-  collectionSize: number = this.users.length;
 
-  constructor(private usersService: UsersService) {
-    this.refreshUsers();
-  }
+  displayedColumn = ['position', 'name', 'age', 'company'];
+  dataSource!: MatTableDataSource<User>;
 
-  refreshUsers(): void {
-    this.filterdUsers = this.users
-      .map((user, index) => ({ ...user, id: index + 1 }))
-      .slice(
-        (this.page - 1) * this.pageSize,
-        (this.page - 1) * this.pageSize + this.pageSize
-      );
-  }
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(private usersService: UsersService) {}
 
   ngOnInit(): void {
     this.usersService.getUsers().subscribe((usersData) => {
-      this.users = usersData;
-      this.refreshUsers();
+      this.filterdUsers = usersData;
+      this.dataSource = new MatTableDataSource(this.filterdUsers);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
     });
   }
 
   applyFilter(event: Event): void {
-    let serachTerm = (event.target as HTMLInputElement).value;
-    serachTerm = serachTerm.toLowerCase();
+    const serachTerm = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = serachTerm.trim().toLowerCase();
 
-    this.filterdUsers = this.users.filter((user) =>
-      user.name.toLowerCase().includes(serachTerm)
-    );
-
-    this.sortUsers(this.sortOrder);
-  }
-
-  sortUsers(sortValue: string): void {
-    this.sortOrder = sortValue;
-
-    switch (this.sortOrder) {
-      case 'ageLowHigh': {
-        console.log('ageLowHigh');
-        this.filterdUsers.sort((a, b) => a.age - b.age);
-
-        break;
-      }
-      case 'ageHighLow': {
-        console.log('ageHighLow');
-        this.filterdUsers.sort((a, b) => b.age - a.age);
-        break;
-      }
-      case 'byName': {
-        console.log('byName');
-        this.filterdUsers.sort((a, b) => a.name.localeCompare(b.name));
-      }
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
     }
   }
 }
