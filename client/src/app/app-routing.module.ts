7bb4886265e88320036f37eb44c18b0458face55
daniel diamant <danieldia44@gmail.com>
2024-01-03 12:33:44 +0200

    register login components

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeHeadComponent } from './components/home/home-head/home-head.component';
 import { UsersTableComponent } from './components/users/users-table/users-table.component';
 import { AboutPageComponent } from './components/about/about-page/about-page.component';
+import { AuthLoginComponent } from './components/auth/auth-login/auth-login.component';
+import { AuthRegisterComponent } from './components/auth/auth-register/auth-register.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeHeadComponent },
   { path: 'about', component: AboutPageComponent },
   { path: 'admin', component: UsersTableComponent },
+  { path: 'login', component: AuthLoginComponent },
+  { path: 'register', component: AuthRegisterComponent },
 ];
 
 @NgModule({
